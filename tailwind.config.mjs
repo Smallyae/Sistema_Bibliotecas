1d1a35ea984e3d1a63be74b5366a593b4316012a
Yae <hitomiyae56@gmail.com>
2024-08-12 13:54:53 -0500

    Add: Colores y espaciados nuevos a tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,14 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: { main: ["Inter", "sans-serif"] },
-    extend: {},
+    extend: {
+      colors: {
+        azul: "#16304E",
+      },
+      spacing: {
+        "30p": "30px",
+      },
+    },
   },
   plugins: [],
 };
